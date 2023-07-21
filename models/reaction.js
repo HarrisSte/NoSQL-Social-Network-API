f0f5a5bc69650afc0569ed0adb573b230134e57d
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-07-21 12:12:50 -0400

    added most of schema - working on reac.ID

--- models/reaction.js
+++ models/reaction.js
@@ -1,4 +1,24 @@
 const { Schema, model } = require('mongoose');
 
+//Schema to create Reaction model.
+const reactionSchema = new Schema(
+    {
+    readtionId: ,
+
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+)
 
 module.exports = Reaction;
